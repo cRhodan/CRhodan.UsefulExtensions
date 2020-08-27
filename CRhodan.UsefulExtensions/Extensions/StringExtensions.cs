27705699a6c02fa5fa4353f3c1becc315d79bd2b
Chris Rhodan <crhodan@deloitte.com.au>
2020-08-27 16:10:47 +1000

    initial

---
+++ CRhodan.UsefulExtensions/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CRhodan.UsefulExtensions.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Returns the currently executing Method's name - like nameof(MethodName) but more robust.
+        /// Usage is this.GetMethodName()
+        /// </summary>
+        public static string GetMethodName(this object type, [CallerMemberName] string caller = null)
+        {
+            return type.GetType().FullName + "." + caller;
+        }
+    }
+}
