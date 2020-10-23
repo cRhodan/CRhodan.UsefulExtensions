c851fa9a58e8a6b1764cf1d9b024b04b0ec62f78
Chris Rhodan <crhodan@deloitte.com.au>
2020-10-23 14:32:14 +1000

    Added GetAge for calculating a person's age based on DateOfBirth and a supplied time

---
+++ CRhodan.UsefulExtensions.Tests/ExtensionTests/DateExtensions/GetAgeTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CRhodan.UsefulExtensions.Extensions;
+using NUnit.Framework;
+
+namespace CRhodan.UsefulExtensions.Tests.ExtensionTests.DateExtensions
+{
+    [TestFixture]
+    public class GetAgeTests
+    {
+        [Test]
+        public void SuppliedDateBeforeDateOfBirth_ThrowException()
+        {
+            var dateOfBirth = DateTime.Now.AddDays(-1);
+            var currentTime = DateTime.Now.AddDays(-2);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => dateOfBirth.GetAge(currentTime));
+        }
+        
+        [TestCase("01/01/2018", "01/02/2018", 0)]
+        [TestCase("02/02/2018", "02/01/2019", 0)]
+        [TestCase("02/02/2018", "02/02/2019", 1)]
+        [TestCase("02/02/2018", "02/03/2019", 1)]
+        [TestCase("12/11/1989", "12/25/2019", 30)]
+        [TestCase("12/11/1989", "12/25/2019", 30)]
+        [TestCase("12/11/1989", "12/11/2020", 31)]
+        public void GetAge(DateTime dateOfBirth, DateTime suppliedDate, int expectedAge)
+        {
+            var age = dateOfBirth.GetAge(suppliedDate);
+            Assert.AreEqual(expectedAge, age);
+        }
+    }
+}
---
+++ CRhodan.UsefulExtensions/Extensions/DateExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CRhodan.UsefulExtensions.Extensions
+{
+    public static class DateExtensions
+    {
+        /// <summary>
+        /// Calculates a person's age based on their date of birth and a supplied date (usually now, but not necessarily)
+        /// </summary>
+        /// <param name="dateOfBirth">The person's date of birth</param>
+        /// <param name="at">The date to calculate their age at</param>
+        /// <returns>Their age</returns>
+        /// <exception cref="ArgumentOutOfRangeException">If the date of calculation is before their date of birth</exception>
+        public static int GetAge(this DateTime dateOfBirth, DateTime at)
+        {
+            if(at < dateOfBirth) throw new ArgumentOutOfRangeException(nameof(at), "Date to check age at cannot be before their date of birth");
+
+            var alreadyHadBirthday = dateOfBirth.Month < at.Month || dateOfBirth.Month == at.Month && dateOfBirth.Day <= at.Day;
+
+            var age = at.Year - dateOfBirth.Year;
+
+            if (!alreadyHadBirthday) age--;
+
+            return age;
+        }
+    }
+}
